8c0df0242eb1d40517c2dbb076efa85d710522ba
rakibjrb <rakibjrb572157@gmail.com>
2023-11-13 10:11:33 +0600

    added call us section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "./HomeComponents/Banner/Banner";
 import BistroBoss from "./HomeComponents/BistroBoss/BistroBoss";
+import CallUs from "./HomeComponents/CallUs/CallUs";
 import OrderOnline from "./HomeComponents/OrderOnline/OrderOnline";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <div className="max-w-7xl mx-auto">
         <OrderOnline />
         <BistroBoss />
+        <CallUs />
       </div>
     </>
   );
---
+++ src/Pages/Home/HomeComponents/CallUs/CallUs.jsx
@@ -0,0 +1,11 @@
+const CallUs = () => {
+  return (
+    <div className="bg-[#000000e9] flex justify-center items-center py-20 mb-32">
+      <h2 className="text-white w-4/5 text-xl md:text-5xl text-center">
+        Call Us: +88 0192345678910
+      </h2>
+    </div>
+  );
+};
+
+export default CallUs;
