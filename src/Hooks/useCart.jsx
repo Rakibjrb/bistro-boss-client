6d6624bdb9250d80f8da4502b1afdb6bc61264bb
rakibjrb <rakibjrb572157@gmail.com>
2023-11-16 12:08:24 +0600

    get cart data depend on logged user

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "./useAxios";
+import useAuth from "./useAuth";
 
 const useCart = () => {
   const axios = useAxios();
+  const { user } = useAuth();
 
   const { data: cartItems = [] } = useQuery({
-    queryKey: ["getcartItems"],
+    queryKey: ["getcartItems", user?.email],
     queryFn: async () => {
-      const res = await axios.get("/cart");
+      const res = await axios.get(`/cart?email=${user?.email}`);
       return res.data;
     },
   });
